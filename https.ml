567537f6a4abd8967614aacd67eb19372a42e115
RadioNoiseE <j18516785606@icloud.com>
2023-10-25 17:52:08 +0800

    general request func with ml

--- https.ml
+++ https.ml
@@ -6,3 +6,19 @@
 
 external ml_request: string -> string -> string -> string = "ocaml_request"
 external ml_fetch: string -> string -> string -> string -> string = "ocaml_fetch"
+
+exception Unexpected_return_value
+
+type data_desc = { url: string; referer: string }
+type resource_desc = { url: string; referer: string; filename: string }
+type record =
+  | Data of data_desc
+  | Resource of resource_desc
+
+let transfer record cookie =
+  let responce = match record with
+    | Data { url; referer } ->
+      ml_request url referer cookie
+    | Resource { url; referer; filename } ->
+      ml_fetch url referer cookie filename
+  in (if responce <> "" then raise Unexpected_return_value else responce)
